ec918e4e7f51cb08f229228e3f6809688e3d71db
Ayomide Oladele <103321800+Heartz00@users.noreply.github.com>
2024-12-28 10:54:56 +0100

    Update App.js

--- healthmania/src/App.js
+++ healthmania/src/App.js
@@ -1,8 +1,19 @@
 import React from "react";
-import HomePage from "./components/HomePage";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Use updated syntax
+import HomePage from "./components/HomePage"; // Import your existing homepage
+import GlucoSensePage from "./components/GlucoSense"; // Import your GlucoSensePage
+
+
 
 function App() {
-  return <HomePage />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} /> {/* Home page route */}
+        <Route path="/glucosense" element={<GlucoSensePage />} /> {/* GlucoSense page */}
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
